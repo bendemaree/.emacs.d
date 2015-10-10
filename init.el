2aae822606b98f83c63c740ec0bc387c07418a2b
Ben Demaree <ben.demaree@ave81.com>
2015-10-10 18:34:06 -0500

    Rework packages and add magit, flycheck.

--- init.el
+++ init.el
@@ -1,15 +1,35 @@
-(require 'package)
+;;; init.el --- main config entry point
+;;; Commentary:
+;;; Code:
 
+;;; Packages
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
+(add-to-list 'load-path (locate-user-emacs-file "vendor/use-package"))
+(require 'use-package)
+
+;; Allow (require) for ELPA packages in init.el
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-(require 'evil)
-(evil-mode t)
+(use-package evil
+  :load-path "vendor/evil"
+  :init
+  (evil-mode t))
 
+(use-package magit)
+
+(use-package flycheck
+  :commands flycheck-mode
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'flycheck-mode))
+
+;;; Theme
 (load-theme 'base16-tomorrow-dark t)
 
+;;; Editing
 (setq-default
  ;; Never use tabs
  indent-tabs-mode nil
@@ -24,26 +44,5 @@
  scroll-conservatively 10000
  scroll-preserve-screen-position 1)
 
-(defun ensure-package-installed (&rest packages)
-    "Assure every package is installed, ask for installation if it’s not.
-
-Return a list of installed packages or nil for every skipped package."
-    (mapcar
-     (lambda (package)
-       (if (package-installed-p package)
-	   nil
-	 (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-	     (package-install package)
-	   package)))
-     packages))
-
-;; Make sure to have downloaded archive description.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-;; Activate installed packages
-(package-initialize)
-
-(ensure-package-installed 'evil
-			  'iedit
-                          'base16-theme)
+(provide 'init)
+;;; init.el ends here
